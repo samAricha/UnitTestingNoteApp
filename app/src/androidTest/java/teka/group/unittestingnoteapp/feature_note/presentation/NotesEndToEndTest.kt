06c6fedac65f1d300a19a1ab2d4bbc96e5e8eef1
samAricha <samaricha001@gmail.com>
2024-08-21 09:43:42 +0300

    feat(saveNewNotes_orderByTitleDescending test): testing the creation of new notes and then arranging them in descending order by title.

--- app/src/androidTest/java/teka/group/unittestingnoteapp/feature_note/presentation/NotesEndToEndTest.kt
+++ app/src/androidTest/java/teka/group/unittestingnoteapp/feature_note/presentation/NotesEndToEndTest.kt
@@ -2,8 +2,12 @@ package teka.group.unittestingnoteapp.feature_note.presentation
 
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertTextContains
+import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.isDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
@@ -92,18 +96,59 @@ class NotesEndToEndTest {
         composeRule.onNodeWithContentDescription("Save note").performClick()
 
         //on notes screen ensure our title and content is displayed
-        composeRule.onNodeWithText("PROGRAMMING").isDisplayed()
-        composeRule.onNodeWithText("coding").isDisplayed()
+        composeRule.onNodeWithText("PROGRAMMING").assertIsDisplayed()
+        composeRule.onNodeWithText("coding").assertIsDisplayed()
 
         //click on created note to navigate to AddNote Screen for editing
         composeRule.onNodeWithText("PROGRAMMING").performClick()
+
+        // Make sure title and content text fields contain note title and content
+        composeRule
+            .onNodeWithTag(TestTags.ADD_TITLE_TXT_FIELD)
+            .assertTextEquals("PROGRAMMING")
+        composeRule
+            .onNodeWithTag(TestTags.ADD_CONTENT_TXT_FIELD)
+            .assertTextEquals("coding")
 
         //on navigation to AddNote Screen edit title and save
         composeRule.onNodeWithTag(TestTags.ADD_TITLE_TXT_FIELD).performTextInput("title")
         composeRule.onNodeWithContentDescription("Save note").performClick()
 
         //ensure title and content is updated
-        composeRule.onNodeWithText("PROGRAMMINGtitle").isDisplayed()
-        composeRule.onNodeWithText("coding").isDisplayed()
+        composeRule.onNodeWithText("titlePROGRAMMING").assertIsDisplayed()
+        composeRule.onNodeWithText("coding").assertIsDisplayed()
+    }
+
+    @Test
+    fun saveNewNotes_orderByTitleDescending(){
+        for(i in 1..3) {
+            //click on FAB to navigate to AddNote Screen
+            composeRule.onNodeWithContentDescription("Add note").performClick()
+            //adding title and content to a new note and save
+            composeRule.onNodeWithTag(TestTags.ADD_TITLE_TXT_FIELD).performTextInput("PROGRAMMING$i")
+            composeRule.onNodeWithTag(TestTags.ADD_CONTENT_TXT_FIELD).performTextInput("coding$i")
+            composeRule.onNodeWithContentDescription("Save note").performClick()
+        }
+
+        //on notes screen ensure our title and content is displayed
+        composeRule.onNodeWithText("PROGRAMMING1").assertIsDisplayed()
+        composeRule.onNodeWithText("coding1").assertIsDisplayed()
+        composeRule.onNodeWithText("PROGRAMMING2").assertIsDisplayed()
+        composeRule.onNodeWithText("coding2").assertIsDisplayed()
+        composeRule.onNodeWithText("PROGRAMMING3").assertIsDisplayed()
+        composeRule.onNodeWithText("coding3").assertIsDisplayed()
+
+        //set up order in descending
+        composeRule.onNodeWithContentDescription("Sort").performClick()
+        composeRule.onNodeWithContentDescription("Title").performClick()
+        composeRule.onNodeWithContentDescription("Descending").performClick()
+
+        composeRule.onAllNodesWithTag(TestTags.NOTE_ITEM)[0].assertTextContains("PROGRAMMING3")
+        composeRule.onAllNodesWithTag(TestTags.NOTE_ITEM)[0].assertTextContains("coding3")
+        composeRule.onAllNodesWithTag(TestTags.NOTE_ITEM)[1].assertTextContains("PROGRAMMING2")
+        composeRule.onAllNodesWithTag(TestTags.NOTE_ITEM)[1].assertTextContains("coding2")
+        composeRule.onAllNodesWithTag(TestTags.NOTE_ITEM)[2].assertTextContains("PROGRAMMING1")
+        composeRule.onAllNodesWithTag(TestTags.NOTE_ITEM)[2].assertTextContains("coding1")
+
     }
 }
--- app/src/main/java/teka/group/unittestingnoteapp/core/util/TestTags.kt
+++ app/src/main/java/teka/group/unittestingnoteapp/core/util/TestTags.kt
@@ -5,7 +5,6 @@ object TestTags {
     const val ORDER_SECTION = "ORDER_SECTION"
     const val ADD_TITLE_TXT_FIELD = "ADD_TITLE_TXT_FIELD"
     const val ADD_CONTENT_TXT_FIELD = "ADD_CONTENT_TXT_FIELD"
-
-
+    const val NOTE_ITEM = "NOTE_ITEM"
 
 }
